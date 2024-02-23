b7f6385282d6ab14b59124725e3a74ecd5c4025e
vladimirovicp <vladimirovicp70@gmail.com>
2024-02-24 00:02:36 +0400

    fix: поправил настройки

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.ts'),
     mode: 'development',
     module: {
         rules: [
@@ -14,14 +14,13 @@ const baseConfig = {
                 use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
             {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                include: [path.resolve(__dirname, 'src')],
+                test: /\.ts$/i,
+                use: 'ts-loader'
             },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts','.js'],
     },
     output: {
         filename: 'index.js',
